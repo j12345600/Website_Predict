c03024588c21a619859d81b33b006c38d082301f
Edison Huang <a123j2010@hotmail.com>
2017-01-06 14:26:46 +0000

    adjust gen_station_list

--- gen_station_list.js
+++ gen_station_list.js
@@ -5,12 +5,24 @@ moment.defaultFormat='YYYY-MM-DD HH:mm:ss';
 var dbsc = require('./secret');
 var connection = mysql.createConnection(dbsc);
 connection.connect();
+var processArray = function(array){
+	var arrayLength = array.length;
+	var result = {};
+	for (var i = 0; i < arrayLength; i++) {
+		result[array[i].sno.toString()]={
+			"sname":array[i].sname,
+			"lat":array[i].lat,
+			"lng":array[i].lng
+		};
+	}
+	return result;
+};
 
 query = "SELECT sno, sname, lat, lng FROM Station_Info WHERE 1";
 connection.query(query,function(err,rows){
     if(err) console.log(err);
     else if(rows!=undefined){
-        fs.writeFile(__dirname+'/public/data/station_info.json',JSON.stringify(rows),'utf-8',(err)=>{
+        fs.writeFile(__dirname+'/public/data/station_info.json',JSON.stringify(processArray(rows)),'utf-8',(err)=>{
             if(err) throw err;
             console.log('Station Info Is Saved!!'+moment().format());
         });

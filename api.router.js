f420a88c0428ea0ed628b1d6afdb46eab3454ad8
Edison Huang <a123j2010@hotmail.com>
2017-01-06 21:35:28 +0800

    add app.js

--- api.router.js
+++ api.router.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 // define the home page route
 router.get('/station_info', function(req, res) {
-  res.send('station_info');
+  res.setHeader('content-type', 'application/json');
+  res.sendFile(__dirname+'/public/data/station_info.json');
 });
 router.get('/station',function(req,res){
     res.send(req.query.id);
